d0004c5ceed3af815b416019de66ddec0a7a0c6a
那达 <youxiang.lyx@alipay.com>
2018-06-20 14:25:55 +0800

    updated tsd

--- lib/core/index.d.ts
+++ lib/core/index.d.ts
@@ -1,5 +1,6 @@
 import { matchRoutes } from 'react-router-config';
 import routes from './routes';
+import { Route } from 'react-router-dom';
 
 /**
  * util to load async component
@@ -23,10 +24,20 @@ export function noop() {}
 
 /**
  * get all matching routes for specific url
- * @param {string} path current path, can use router's location.pathname
+ * @param {string} path current path, can use router's location.pathname, or match.url
  */
-export function getMatchedRoutes(path) {
+export function getMatchedRoutes(path: string) {
   return matchRoutes(routes, path);
+}
+
+/**
+ * get metadata from routes, search from leaf to root
+ * @param {string} path current path, can use router's location.pathname, or match.url
+ * @param {(route: Route) => any} filter filter function to get meta
+ */
+export function getMatchedRoutesMeta(path: string, filter: (route: Route) => any) {
+  const matchedRoutes = getMatchedRoutes(path);
+  return getMetaFromMatchedRoutes(matchedRoutes, filter);
 }
 
 export { connect } from 'react-redux';
